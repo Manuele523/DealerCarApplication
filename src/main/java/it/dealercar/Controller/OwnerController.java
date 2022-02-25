a841eb30cec6a7b2d497e148d78111b759f66720
Manuele523 <montrasi.manuele523@gmail.com>
2022-02-25 09:57:13 +0100

    Add CrossOrigin in controller

--- src/main/java/it/dealercar/Controller/OwnerController.java
+++ src/main/java/it/dealercar/Controller/OwnerController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Owner/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class OwnerController {
 
     @Autowired
--- src/main/java/it/dealercar/Controller/ServiceController.java
+++ src/main/java/it/dealercar/Controller/ServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Service/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ServiceController {
 
     @Autowired
